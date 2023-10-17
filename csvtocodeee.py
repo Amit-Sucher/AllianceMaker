0e71f28f766a33c880617d54500dee6a8cdbf4da
AmitSu <sucher.amit@gmail.com>
2023-10-18 02:26:45 +0300

    so today I reworked some of the functions on the algorithm TODO: right now the alliance captain is picked but also picking (like 1577 or 3339) need to fix that

--- csvtocodeee.py
+++ csvtocodeee.py
@@ -11,7 +11,7 @@ def get_csv_data(file_name):
     with open(file_name, 'r', encoding='utf-8') as file:
         reader = csv.DictReader(file)
         for row in reader:
-            team_number = row['\ufeffTeam']  # use '\ufeffTeam' instead of 'Team'
+            team_number = row['\ufeffTeam']
             if team_number not in team_data:
                 team_data[team_number] = {}
             for column, value in row.items():
@@ -33,30 +33,49 @@ def calculate_bonus(data):
     if ('Auto Cones High' in data and float(data['Auto Cones High']) > 0 or
         'Auto Cones Mid' in data and float(data['Auto Cones Mid']) > 0 or
         'Auto Cones Low' in data and float(data['Auto Cones Low']) > 0) and \
-       ('Auto Cubes High' in data and float(data['Auto Cubes High']) > 0 or
-        'Auto Cubes Mid' in data and float(data['Auto Cubes Mid']) > 0 or
-        'Auto Cubes Low' in data and float(data['Auto Cubes Low']) > 0):
-           bonus_points += BONUS_AUTO
+            ('Auto Cubes High' in data and float(data['Auto Cubes High']) > 0 or
+             'Auto Cubes Mid' in data and float(data['Auto Cubes Mid']) > 0 or
+             'Auto Cubes Low' in data and float(data['Auto Cubes Low']) > 0):
+        bonus_points += BONUS_AUTO
 
     if ('Teleop Cones High' in data and float(data['Teleop Cones High']) > 0 or
         'Teleop Cones Mid' in data and float(data['Teleop Cones Mid']) > 0 or
         'Teleop Cones Low' in data and float(data['Teleop Cones Low']) > 0) and \
-       ('Teleop Cubes High' in data and float(data['Teleop Cubes High']) > 0 or
-        'Teleop Cubes Mid' in data and float(data['Teleop Cubes Mid']) > 0 or
-        'Teleop Cubes Low' in data and float(data['Teleop Cubes Low']) > 0):
-            bonus_points += BONUS_TELEOP
+            ('Teleop Cubes High' in data and float(data['Teleop Cubes High']) > 0 or
+             'Teleop Cubes Mid' in data and float(data['Teleop Cubes Mid']) > 0 or
+             'Teleop Cubes Low' in data and float(data['Teleop Cubes Low']) > 0):
+        bonus_points += BONUS_TELEOP
     return bonus_points
 
 
-def pick_team(alliance, teams_remaining, team_data):
+def calculate_total_score(team_data):
+    scores = [calculate_points(team_data, category) for category in CATEGORIES]
+    scores.append(calculate_bonus(team_data))
+
+    rank = int(team_data["Rank"])
+    scores.append(1 / rank)  # higher rank contributes more
+
+    return sum(scores)
+
+
+def calculate_score_without_rank(team_data):
+    scores = [calculate_points(team_data, category) for category in CATEGORIES]
+    scores.append(calculate_bonus(team_data))
+    return sum(scores)
+
+
+def pick_team(alliance, teams_remaining, team_data, current_alliances):
     print(f"Current alliance: {alliance}")
     print(f"Available keys in team_data: {team_data.keys()}")
     robot = team_data[alliance[-1]]
 
     evaluated_partners = evaluate_partners(robot, teams_remaining, team_data)
 
-
+    # the next entry is alliance leader which we need to avoid in the picked up teams
     best_partner = evaluated_partners[0]
+    while best_partner in [item for sublist in current_alliances for item in sublist]:
+        evaluated_partners.pop(0)
+        best_partner = evaluated_partners[0]
 
     print(f"Picked partner: {best_partner}")
     return best_partner
@@ -68,65 +87,55 @@ def evaluate_partners(robot, potential_partners_team_numbers, team_data):
         partner = team_data[team_number]
         scores = {}
         for category in CATEGORIES:
-            # store the score difference for each category
             scores[category] = calculate_points(robot, category) - calculate_points(partner, category)
 
-        # find the category with max score difference, i.e. max weakness
         max_score_diff_category = max(scores, key=scores.get)
 
-        # total score is same as your previous implementation
-        total_score = sum(calculate_points(partner, category) for category in CATEGORIES) + calculate_bonus(partner)
+        total_score = calculate_score_without_rank(partner)
 
-        # store team_number and its total score and max score difference category
         evaluations.append((total_score, max_score_diff_category, team_number))
 
-    # Sort by total score descending, and then by max weakness
-    # so that partners with same total score are then sorted by their max weakness
-    evaluations.sort(key=lambda x: (-x[0], x[1]))
+    key = lambda x: (-x[0], x[1])  # do not consider rank in picking process
+    evaluations.sort(key=key)
 
-    # Return list of team_numbers of best partners
     return [team_number for _, _, team_number in evaluations]
 
 
 def print_alliances(alliances):
     for i, alliance in enumerate(alliances):
-        print(f'Alliance {i+1}:')
+        print(f'Alliance {i + 1}:')
         for robot in alliance:
             print(f' - Robot {robot}')
         print()
 
 
 def create_alliances(team_data):
-    ranked_teams = sorted(team_data.keys(), key=lambda team: calculate_total_score(team_data[team]), reverse=True)
+    key = lambda team: int(team_data[team]['Rank'])
+    ranked_teams = sorted(team_data.keys(), key=key)
 
     alliances = []
-
     alliance_leaders = ranked_teams[:8]
-
-    teams_remaining = ranked_teams[8:]
+    all_teams = set(ranked_teams)  # we use a set here for efficiency
 
     for leader in alliance_leaders:
         alliance = [leader]
+        all_teams.discard(leader)
 
         for _ in range(1):
-            next_robot = pick_team(alliance, teams_remaining, team_data)
-            alliance.append(next_robot)
-            teams_remaining.remove(next_robot)
+            if all_teams:  # still has robot to pick
+                next_robot = pick_team(alliance, list(all_teams), team_data, alliances)
+                alliance.append(next_robot)
+                all_teams.discard(next_robot)
 
         alliances.append(alliance)
 
-    # Now do the second round 8-1 rank order
     for alliance in reversed(alliances):
-        next_robot = pick_team(alliance, teams_remaining, team_data)
-        alliance.append(next_robot)
-        teams_remaining.remove(next_robot)
+        if all_teams:  # still has robot to pick
+            next_robot = pick_team(alliance, list(all_teams), team_data, alliances)
+            alliance.append(next_robot)
+            all_teams.discard(next_robot)
 
     return alliances
-
-def calculate_total_score(team_data):
-    scores = [calculate_points(team_data, category) for category in CATEGORIES]
-    scores.append(calculate_bonus(team_data))
-    return sum(scores)
 
 
 def main():
